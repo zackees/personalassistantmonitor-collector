f714a77d3983df62fb94693db0d9e61fb41505f9
zackees <z@zackees.com>
2022-12-11 12:46:42 -0800

    adds upload

--- personalmonitor_collector/app.py
+++ personalmonitor_collector/app.py
@@ -4,10 +4,13 @@
 
 import os
 from datetime import datetime
+from tempfile import TemporaryDirectory
+
+# import shutil
 import uvicorn  # type: ignore
 from fastapi.middleware.cors import CORSMiddleware  # type: ignore
 from fastapi.responses import RedirectResponse, JSONResponse  # type: ignore
-from fastapi import FastAPI  # type: ignore
+from fastapi import FastAPI, UploadFile, File  # type: ignore
 
 from personalmonitor_collector.version import VERSION
 
@@ -43,6 +46,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+CHUNK_SIZE = 1024 * 64
+
+
+async def async_download(src: UploadFile, dst: str) -> None:
+    """Downloads a file to the destination."""
+    with open(dst, mode="wb") as filed:
+        while (chunk := await src.read(CHUNK_SIZE)) != b"":
+            filed.write(chunk)
+    await src.close()
+
 
 @app.get("/", include_in_schema=False)
 async def route_index() -> RedirectResponse:
@@ -53,6 +66,16 @@ async def route_index() -> RedirectResponse:
 @app.get("/get")
 async def route_get() -> JSONResponse:
     """TODO - Add description."""
+    return JSONResponse({"hello": "world"})
+
+
+@app.post("/upload")
+async def route_upload(file: UploadFile = File(...)) -> JSONResponse:
+    """TODO - Add description."""
+    with TemporaryDirectory() as temp_dir:
+        temp_path: str = os.path.join(temp_dir, file.filename)
+        await async_download(file, temp_path)
+        # shutil.move(temp_path, final_path)
     return JSONResponse({"hello": "world"})
 
 
